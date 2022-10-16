69c8f1a4f2f56de93f0ebcbb6f081b20dbeb3944
unknown <carter.adrian@gmail.com>
2022-10-16 19:19:11 -0400

    set time

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
-$(document).ready(function () {
-   $('.saveBtn').on('click', function () {
+$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+   //display current day & time.
+   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+   //assign saveBtn click listener for user input and time stamp??
+   $(".saveBtn").on("click", function () {
+       //get nearby values.
+       console.log(this);
+       var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+       var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-   }); 
-});
-
-var today = moment();
-$("#currentDay").text(today.format("MMMM Do YYYY, h:mm:ss a"));
