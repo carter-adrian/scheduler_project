2a0c2a31fec61c1a57b84a3be57c9be19af8178e
unknown <carter.adrian@gmail.com>
2022-10-16 19:29:46 -0400

    set local storage for each hour

--- Develop/script.js
+++ Develop/script.js
@@ -8,3 +8,19 @@ $(document).ready(function () {// tells engine to load 1)html & 2)css first.
        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
+       localStorage.setItem(time, text);
+   }) 
+//    load any saved data, possible for loop?
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+function hourTracker() {
+    
+}
