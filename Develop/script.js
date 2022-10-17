ee1729341a0a0f83265f6e583521050a558e03fe
unknown <carter.adrian@gmail.com>
2022-10-17 16:26:45 -0400

    troubleshooting save issue

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,9 @@
-$(document).ready(function () {// tells engine to load 1)html & 2)css first.
-    //display current day & time.
+$(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
-    //assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
-        //get nearby values.
         console.log(this);
-        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
 
         //set items in local storage.
         localStorage.setItem(time, text);
